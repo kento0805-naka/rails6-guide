6bdf67d52cd690f2289e1bba5148ec585849adb9
kento <kentz.eva.a.r.xxx@ezweb.ne.jp>
2020-08-15 12:15:11 +0000

    シードデータの投入

---
+++ db/seeds/development/staff_members.rb
@@ -0,0 +1,9 @@
+StaffMember.create!(
+    email: "example@gmail.com",
+    family_name: "中村",
+    given_name: "健人",
+    family_name_kana: "ナカムラ",
+    given_name_kana: "ケント",
+    password: "password",
+    start_date: Date.today
+)
