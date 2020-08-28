7e65358becf2d72d5fa93438f1ea26076e6441b7
kento <kentz.eva.a.r.xxx@ezweb.ne.jp>
2020-08-28 09:04:21 +0000

    route ホスト名の制約

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :staff do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+  constraints host: "baukis2.example.com" do
+    namespace :staff, path: "" do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :account, except: [ :new, :create, :destroy ]
+    end
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+    resources :staff_members
   end
 
   namespace :customer do
